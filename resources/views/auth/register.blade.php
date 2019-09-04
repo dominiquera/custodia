f18000706b1d86589b35184c0388286d747fd859
Unknown <deanhopkins@gmail.com>
2019-09-05 00:57:35 +0100

    layout styling

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="ui container" style="padding-top:50px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <h1 class="ui header">{{ __('Register') }}</h1>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" class="ui form" action="{{ route('register') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="field">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -25,7 +26,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="field">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -39,7 +40,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="field">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -53,7 +54,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="field">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
@@ -61,9 +62,9 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="field row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="ui button primary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
