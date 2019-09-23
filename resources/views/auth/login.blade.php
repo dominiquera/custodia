5d5b6d71bb8ae1a2821432a145664f0473c451ad
aednzxy <deanhopkins@live.co.uk>
2019-09-23 22:47:07 +0100

    Layout templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-<div class=" ui container" style="padding-top:50px;">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <h1 class="ui header">{{ __('Login') }}</h1>
+    <div class=" ui container" style="padding-top:50px;">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <h1 class="ui header">{{ __('Login') }}</h1>
 
-                <div class="card-body">
-                    <form method="POST" class="ui form" action="{{ route('login') }}">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" class="ui form" action="{{ route('login') }}">
+                            @csrf
 
-                        <div class="field">
-                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            <div class="field">
+                                <label for="email">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                @error('email')
+                                    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label for="password">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <div class="ui checkbox">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                    @enderror
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="ui button primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="field">
+                                <label for="password">{{ __('Password') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="field">
+                                <div class="ui checkbox">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="ui button primary">
+                                        {{ __('Login') }}
+                                    </button>
 
                                 <!-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif -->
+                                            </a>
+@endif -->
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,55 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/semantic.min.js') }}"></script>
     <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        <main>
+        <nav>
+            <div class="ui container" style="padding-top:50px">
+                <div class="ui menu">
+
+                    <a class="item" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+
+                    <div class="right menu">
+                        @guest
+                            <a class="item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            @if (Route::has('register'))
+                                <a class="item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            @endif
+                        @else
+                            <a class="item" href="{{ url('/logout') }}"
+                               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                        @endguest
+                    </div>
+
+                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
+            </div>
+
+        </nav>
+
+        <main class="py-4">
             @yield('content')
         </main>
     </div>
+    <script
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous"></script>
+    @yield('scripts');
 </body>
 </html>
