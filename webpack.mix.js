4fb92601785a1d4ac898b218346944a2d753e928
aednzxy <deanhopkins@live.co.uk>
2019-09-23 21:46:56 +0100

    Semantic UI dependancy

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,4 +17,5 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .copy('semantic/dist/semantic.min.css','public/css/semantic.min.css')
-    .copy('semantic/dist/semantic.min.js','public/js/semantic.min.js');
+    .copy('semantic/dist/semantic.min.js','public/js/semantic.min.js')
+    .copy('semantic/dist/themes','public/css/themes');
