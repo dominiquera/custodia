df41284c446247bc45e3fa3708bf384254df1f7c
aednzxy <deanhopkins@live.co.uk>
2019-10-09 21:16:05 +0100

    Schedule command to run weekly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace Custodia\Console;
 
+use Custodia\Console\Commands\WeeklyScoringCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +27,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command(WeeklyScoringCommand::class)->weekly();
+
     }
 
     /**
