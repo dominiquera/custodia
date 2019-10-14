49458a6a3f82c8f282008169d971214b04bb7f56
Unknown <deanhopkins@gmail.com>
2019-10-14 12:36:44 +0100

    User API endpoints

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,18 @@ Route::post('/v1/users', 'Admin\UserController@apiCreateUser');
 Route::post('/v1/users/{user}/score', 'Admin\UserController@apiSetUserScore');
 Route::get('/v1/users/{user}/score', 'Admin\UserController@apiGetUserScore');
 
+Route::post('/v1/users/{user}/outdoor_spaces', 'Admin\UserController@apiSetOutdoorSpaces');
+Route::get('/v1/users/{user}/outdoor_spaces', 'Admin\UserController@apiGetOutdoorSpaces');
+
+Route::post('/v1/users/{user}/driveways', 'Admin\UserController@apiSetDriveways');
+Route::get('/v1/users/{user}/driveways', 'Admin\UserController@apiGetDriveways');
+
+Route::post('/v1/users/{user}/home_features', 'Admin\UserController@apiSetHomeFeatures');
+Route::get('/v1/users/{user}/home_features', 'Admin\UserController@apiGetHomeFeatures');
+
+Route::post('/v1/users/{user}/mobility_issues', 'Admin\UserController@apiSetMobilityIssues');
+Route::get('/v1/users/{user}/mobility_issues', 'Admin\UserController@apiGetMobilityIssues');
+
 Route::get('/v1/users/{user}/done_maintenance_items', 'Admin\UserController@apiGetUserDoneMaintenanceItems');
 Route::get('/v1/users/{user}/ignored_maintenance_items', 'Admin\UserController@apiGetUserIgnoredMaintenanceItems');
 Route::get('/v1/users/{user}/top_three_maintenance_items_today', 'Admin\UserController@apiGetTop3MaintenanceItemsTodayByUser');
