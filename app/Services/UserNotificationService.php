24189dc0dbae97fac616cfdb8c2a8ab6fcdc3bce
Chris Shank <chris@custodia.com>
2020-02-24 15:00:40 -0500

    added user notification service

---
+++ app/Services/UserNotificationService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Custodia\Services;
+
+use Custodia\User;
+use Illuminate\Support\Facades\Log;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\MulticastSendReport;
+use Kreait\Firebase\Messaging\Notification;
+
+class UserNotificationService
+{
+    /**
+     * @param User   $user
+     * @param string $title
+     * @param string $body
+     * @return void
+     */
+    public function sendNotificationToUser(User $user, string $title, string $body) {
+        $tokens = $user->tokens()->where('scope', 'fcm_token')->pluck('token')->toArray();
+
+        if (empty($tokens))
+            return; // no notification can be sent
+
+        Log::debug("sending notification", [ $user->email, $title, $body ]);
+        $notification = Notification::create($title, $body);
+
+        $message = CloudMessage::new()
+                               ->withNotification($notification);
+
+        $fcm = app('FirebaseService')->getMessaging();
+
+        /** @var MulticastSendReport|null $report */
+        $report = $fcm->sendMulticast($message, $tokens);
+
+        if ($report->hasFailures()) {
+            foreach ($report->failures()->getItems() as $failure) {
+                print "warning: failed to send to " . $failure->target()->value() . ": " . $failure->error()->getMessage() . "\n";
+                Log::warning("failed to send notification to device", [
+                    'target' => $failure->target()->value(),
+                    'error' => $failure->error()->getMessage()
+                ]);
+            }
+        }
+    }
+}
